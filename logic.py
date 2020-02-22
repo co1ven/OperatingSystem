5e3156f119656c592825a37b973a534b21b348cc
Victor <co1ven@bk.ru>
2020-02-23 01:08:43 +0300

    Almost final version

--- logic.py
+++ logic.py
@@ -1,31 +1,67 @@
 from selenium import webdriver
+from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException
 from selenium.webdriver.firefox.options import Options
+import re
+import json
 
 
 class OperatingSystem:
     URL = "https://vk.com"
-    PROFILE = (r"/home/victor/.mozilla/firefox/yifmws2l.default")
+    opts = Options()
+    opts.profile = (r"/home/victor/.mozilla/firefox/yifmws2l.default")
+
+    def save_data_to_file(self, data, filename="file.json"):
+        """Сохранение данных в файл"""
+        with open(filename, 'w') as file:
+            file.write(data)
+
+    def check_modal_window(self, driver):
+        """Проверка на выпадающее модальное окно"""
+        if driver.find_element_by_id("box_layer").is_displayed():
+            driver.find_element_by_class("box_x_button").click()
+
+    def find_info(self, driver):
+        """Парсинг id, text и image_url новостей"""
+        self.check_modal_window(driver)
+        news = driver.find_elements_by_class_name("feed_row")
+        image_links = []
+        data = []
+        for item in news:
+            try:
+                content = item.find_element_by_class_name("wall_post_text").text
+                urls = re.match(r"(?P<url>https?://[^\s]+)", content)
+            except:
+                content = "Text not exists."
+                urls = "Links are not exist."
+            id = item.find_element_by_tag_name("div").get_attribute("id")[5:]
+            try:
+                if 'showPhoto' in item.find_element_by_class_name("page_post_thumb_wrap").get_attribute("onclick"):
+                    image_links.append(item.find_element_by_class_name("page_post_thumb_wrap"))
+                else:
+                    image_links.append(None)
+            except:
+                pass
+
+            data.append({"content": content, "id": id, "image_url": None, "urls": urls})
+        for i in range(len(image_links)):
+            if image_links[i] == None:
+                continue
+            else:
+                try:
+                    image_links[i].click()
+                    image_url = driver.find_element_by_id("pv_photo").find_element_by_tag_name("img").get_attribute(
+                        "src")
+                    driver.find_element_by_class_name("pv_close_btn").click()
+                    data[i]["image_url"] = image_url
+                except (ElementClickInterceptedException, NoSuchElementException):
+                    pass
+        return json.dumps(data, sort_keys=True, ensure_ascii=False, indent=4, separators=(',', ': '))
+
+
 
     def run(self):
-        opts = Options()
-        opts.profile = self.PROFILE
-        driver = webdriver.Firefox(options=opts)
+        """Запуск программы"""
+        driver = webdriver.Firefox(options=self.opts)
         driver.get(self.URL)
-        result = driver.find_elements_by_xpath("//div[@class='feed_row']")
-        print(dir(result))
-        for item in result:
-            try:
-                post_id = item.find_element_by_xpath("//div[@class=post]").get_attribute("id")
-            except:
-                post_id = "1333"
-            try:
-                text = item.find_element_by_xpath("//div[@class='wall_post_text']")
-            except:
-                text = ""
-            # try:
-            #     image_a = item.find_element_by_xpath("//a[@class='page_post_thumb_wrap image_cover  page_post_thumb_last_column page_post_thumb_last_row']")
-            #     image_a.click()
-            # except:
-            #     image_a = ""
-            print(text.text, post_id)
-        # driver.close()
+        result = self.find_info(driver)
+        self.save_data_to_file(result)
