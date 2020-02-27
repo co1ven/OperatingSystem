61203bee30a312a8b7a2184631bb62c1b9cfedfd
Victor <co1ven@bk.ru>
2020-02-27 21:29:40 +0300

    Stable version

--- logic.py
+++ logic.py
@@ -1,8 +1,10 @@
-from selenium import webdriver
-from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException
-from selenium.webdriver.firefox.options import Options
+from time import sleep
 import re
 import json
+
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.firefox.options import Options
 
 
 class OperatingSystem:
@@ -10,58 +12,70 @@ class OperatingSystem:
     opts = Options()
     opts.profile = (r"/home/victor/.mozilla/firefox/yifmws2l.default")
 
-    def save_data_to_file(self, data, filename="file.json"):
+    def save_data_to_json(self, data, filename="file.json", post_id=True, post_text=True, post_links=True, post_img_links=True):
         """Сохранение данных в файл"""
+        final_data = []
+        for item in data:
+            q = {}
+            q.update(id=item["post_id"]) if post_id else q.update()
+            q.update(text=item["post_text"]) if post_text else q.update()
+            q.update(links=item["post_links"]) if post_links else q.update()
+            q.update(img_links=item["post_image_links"]) if post_img_links else q.update()
+            final_data.append(q)
         with open(filename, 'w') as file:
-            file.write(data)
+            file.write(json.dumps(final_data, sort_keys=True, ensure_ascii=False, indent=3, separators=(',', ': ')))
 
     def check_modal_window(self, driver):
         """Проверка на выпадающее модальное окно"""
         if driver.find_element_by_id("box_layer").is_displayed():
-            driver.find_element_by_class("box_x_button").click()
+            driver.find_element_by_class_name("box_x_button").click()
 
-    def find_info(self, driver):
-        """Парсинг id, text и image_url новостей"""
+    def get_info(self, driver):
+        """Парсинг новостей"""
         self.check_modal_window(driver)
-        news = driver.find_elements_by_class_name("feed_row")
-        image_links = []
+        news = driver.find_element_by_id("feed_rows")
+        # next_news = news.find_element_by_id("feed_rows_next") # Новые новости
+        # unshown_news = news.find_element_by_class_name("feed_row_unshown") # Следующие новости
+        items = news.find_elements_by_class_name("feed_row")
         data = []
-        for item in news:
+        for item in items:
+            # Проверка на содержимое поста (реклама, новость или рекомендованные друзья)
             try:
-                content = item.find_element_by_class_name("wall_post_text").text
-                urls = re.match(r"(?P<url>https?://[^\s]+)", content)
-            except:
-                content = "Text not exists."
-                urls = "Links are not exist."
-            id = item.find_element_by_tag_name("div").get_attribute("id")[5:]
-            try:
-                if 'showPhoto' in item.find_element_by_class_name("page_post_thumb_wrap").get_attribute("onclick"):
-                    image_links.append(item.find_element_by_class_name("page_post_thumb_wrap"))
-                else:
-                    image_links.append(None)
-            except:
-                pass
-
-            data.append({"content": content, "id": id, "image_url": None, "urls": urls})
-        for i in range(len(image_links)):
-            if image_links[i] == None:
-                continue
-            else:
+                post = item.find_element_by_class_name("post")
+                post_id = post.get_attribute("id")
+                post_content = post.find_element_by_class_name("post_content")
+                # Проверка на наличие текста в новости
                 try:
-                    image_links[i].click()
-                    image_url = driver.find_element_by_id("pv_photo").find_element_by_tag_name("img").get_attribute(
-                        "src")
-                    driver.find_element_by_class_name("pv_close_btn").click()
-                    data[i]["image_url"] = image_url
-                except (ElementClickInterceptedException, NoSuchElementException):
-                    pass
-        return json.dumps(data, sort_keys=True, ensure_ascii=False, indent=4, separators=(',', ': '))
-
-
+                    text = post_content.find_element_by_class_name("wall_post_text").text
+                    links = re.findall(r'((https?)?://[^\s]+)', text)
+                except NoSuchElementException:
+                    text = "<-- There are not text. -->"
+                    links = "<-- There are not links. -->"
+                # Проверка на наличие картинки в новости
+                try:
+                    post_image_container = post.find_element_by_class_name("page_post_sized_thumbs")
+                    image_a = post_image_container.find_elements_by_class_name("image_cover")
+                    img_links = []
+                    for a in image_a:
+                        sleep(5)
+                        if 'showPhoto' in a.get_attribute("onclick"):
+                            a.click()
+                            img_link = driver.find_element_by_id("pv_photo").find_element_by_tag_name("img").get_attribute("src")
+                            img_links.append(img_link)
+                            driver.find_element_by_class_name("pv_close_btn").click()
+                except NoSuchElementException:
+                    img_links = "<-- There are not images. -->"
+            except NoSuchElementException:
+                continue
+            data.append({"post_id": post_id, "post_text": text, "post_links": links, "post_image_links": img_links})
+        return data
 
     def run(self):
         """Запуск программы"""
         driver = webdriver.Firefox(options=self.opts)
         driver.get(self.URL)
-        result = self.find_info(driver)
-        self.save_data_to_file(result)
+        result = self.get_info(driver)
+        self.save_data_to_json(result, "file1.json", post_links=False, post_img_links=False)
+        self.save_data_to_json(result, "file2.json", post_text=False, post_links=False)
+        self.save_data_to_json(result, "file3.json", post_text=False, post_img_links=False)
+        driver.close()
