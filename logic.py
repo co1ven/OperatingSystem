2387ea871cae9ef00bf45f95e1acc52ed3116e91
Victor <co1ven@bk.ru>
2020-02-29 23:10:02 +0300

    Added threads

--- logic.py
+++ logic.py
@@ -1,6 +1,8 @@
 from time import sleep
 import re
 import json
+from threading import Thread
+from datetime import datetime
 
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
@@ -11,6 +13,7 @@ class OperatingSystem:
     URL = "https://vk.com"
     opts = Options()
     opts.profile = (r"/home/victor/.mozilla/firefox/yifmws2l.default")
+    is_thread = False
 
     def save_data_to_json(self, data, filename="file.json", post_id=True, post_text=True, post_links=True, post_img_links=True):
         """Сохранение данных в файл"""
@@ -34,8 +37,8 @@ class OperatingSystem:
         """Парсинг новостей"""
         self.check_modal_window(driver)
         news = driver.find_element_by_id("feed_rows")
-        # next_news = news.find_element_by_id("feed_rows_next") # Новые новости
-        # unshown_news = news.find_element_by_class_name("feed_row_unshown") # Следующие новости
+        # next_news = news.find_element_by_id("feed_rows_next") # Следующие новости
+        # unshown_news = news.find_element_by_class_name("feed_row_unshown") # Новые новости
         items = news.find_elements_by_class_name("feed_row")
         data = []
         for item in items:
@@ -49,13 +52,13 @@ class OperatingSystem:
                     text = post_content.find_element_by_class_name("wall_post_text").text
                     links = re.findall(r'((https?)?://[^\s]+)', text)
                 except NoSuchElementException:
-                    text = "<-- There are not text. -->"
-                    links = "<-- There are not links. -->"
+                    text = ""
+                    links = ""
                 # Проверка на наличие картинки в новости
+                img_links = []
                 try:
                     post_image_container = post.find_element_by_class_name("page_post_sized_thumbs")
                     image_a = post_image_container.find_elements_by_class_name("image_cover")
-                    img_links = []
                     for a in image_a:
                         sleep(5)
                         if 'showPhoto' in a.get_attribute("onclick"):
@@ -64,7 +67,7 @@ class OperatingSystem:
                             img_links.append(img_link)
                             driver.find_element_by_class_name("pv_close_btn").click()
                 except NoSuchElementException:
-                    img_links = "<-- There are not images. -->"
+                    continue
             except NoSuchElementException:
                 continue
             data.append({"post_id": post_id, "post_text": text, "post_links": links, "post_image_links": img_links})
@@ -75,7 +78,35 @@ class OperatingSystem:
         driver = webdriver.Firefox(options=self.opts)
         driver.get(self.URL)
         result = self.get_info(driver)
-        self.save_data_to_json(result, "file1.json", post_links=False, post_img_links=False)
-        self.save_data_to_json(result, "file2.json", post_text=False, post_links=False)
-        self.save_data_to_json(result, "file3.json", post_text=False, post_img_links=False)
+        time_start = datetime.now()
+        if self.is_thread:
+            thread1 = Thread(target=self.save_data_to_json, kwargs={
+                "data": result,
+                "filename": "file1.json",
+                "post_links": False,
+                "post_img_links": False
+            })
+            thread2 = Thread(target=self.save_data_to_json, kwargs={
+                "data": result,
+                "filename": "file2.json",
+                "post_text": False,
+                "post_links": False
+            })
+            thread3 = Thread(target=self.save_data_to_json, kwargs={
+                "data": result,
+                "filename": "file3.json",
+                "post_text": False,
+                "post_img_links": False
+            })
+            thread1.start()
+            thread2.start()
+            thread3.start()
+            thread1.join()
+            thread2.join()
+            thread3.join()
+        else:
+            self.save_data_to_json(result, "file1.json", post_links=False, post_img_links=False)
+            self.save_data_to_json(result, "file2.json", post_links=False, post_text=False)
+            self.save_data_to_json(result, "file3.json", post_text=False, post_img_links=False)
+        print(datetime.now() - time_start)
         driver.close()
--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from logic import OperatingSystem
 
 if __name__ == '__main__':
     app = OperatingSystem()
+    app.is_thread = True
     app.run()
