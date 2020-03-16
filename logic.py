91335e0c3320c1a2f84a85a8880cba22b5ea29b6
Victor <co1ven@bk.ru>
2020-03-16 23:18:49 +0300

    Version 2.0

--- logic.py
+++ logic.py
@@ -10,38 +10,28 @@ from selenium.webdriver.firefox.options import Options
 
 
 class OperatingSystem:
-    URL = "https://vk.com"
-    opts = Options()
-    opts.profile = (r"/home/victor/.mozilla/firefox/yifmws2l.default")
-    is_thread = False
 
-    def save_data_to_json(self, data, filename="file.json", post_id=True, post_text=True, post_links=True, post_img_links=True):
-        """Сохранение данных в файл"""
-        final_data = []
-        for item in data:
-            q = {}
-            q.update(id=item["post_id"]) if post_id else q.update()
-            q.update(text=item["post_text"]) if post_text else q.update()
-            q.update(links=item["post_links"]) if post_links else q.update()
-            q.update(img_links=item["post_image_links"]) if post_img_links else q.update()
-            final_data.append(q)
-        with open(filename, 'w') as file:
-            file.write(json.dumps(final_data, sort_keys=True, ensure_ascii=False, indent=3, separators=(',', ': ')))
+    def __init__(self):
+        self.URL = "https://vk.com"
+        opts = Options()
+        opts.profile = (r"/home/victor/.mozilla/firefox/yifmws2l.default")
+        # open browser
+        self.driver = webdriver.Firefox(options=opts)
 
-    def check_modal_window(self, driver):
+    def check_modal_window(self):
         """Проверка на выпадающее модальное окно"""
-        if driver.find_element_by_id("box_layer").is_displayed():
-            driver.find_element_by_class_name("box_x_button").click()
+        if self.driver.find_element_by_id("box_layer").is_displayed():
+            self.driver.find_element_by_class_name("box_x_button").click()
 
-    def get_info(self, driver):
+    def get_info(self):
         """Парсинг новостей"""
-        self.check_modal_window(driver)
-        news = driver.find_element_by_id("feed_rows")
-        # next_news = news.find_element_by_id("feed_rows_next") # Следующие новости
-        # unshown_news = news.find_element_by_class_name("feed_row_unshown") # Новые новости
+        # checking for modal window
+        self.check_modal_window()
+        # parse data
+        news = self.driver.find_element_by_id("feed_rows")
         items = news.find_elements_by_class_name("feed_row")
-        data = []
         for item in items:
+            self.driver.execute_script("arguments[0].scrollIntoView();", item)
             # Проверка на содержимое поста (реклама, новость или рекомендованные друзья)
             try:
                 post = item.find_element_by_class_name("post")
@@ -50,63 +40,59 @@ class OperatingSystem:
                 # Проверка на наличие текста в новости
                 try:
                     text = post_content.find_element_by_class_name("wall_post_text").text
-                    links = re.findall(r'((https?)?://[^\s]+)', text)
+                    links = re.findall(r'((?:https?://)?(?:[\w.]+)\.(?:[a-z]{2,6}\.?)(?:/[\w.]*)*/?)', text)
                 except NoSuchElementException:
                     text = ""
                     links = ""
                 # Проверка на наличие картинки в новости
-                img_links = []
                 try:
+                    img_links = []
                     post_image_container = post.find_element_by_class_name("page_post_sized_thumbs")
                     image_a = post_image_container.find_elements_by_class_name("image_cover")
                     for a in image_a:
                         sleep(5)
                         if 'showPhoto' in a.get_attribute("onclick"):
                             a.click()
-                            img_link = driver.find_element_by_id("pv_photo").find_element_by_tag_name("img").get_attribute("src")
+                            img_link = self.driver.find_element_by_id("pv_photo").find_element_by_tag_name(
+                                "img").get_attribute("src")
                             img_links.append(img_link)
-                            driver.find_element_by_class_name("pv_close_btn").click()
+                            self.driver.find_element_by_class_name("pv_close_btn").click()
                 except NoSuchElementException:
-                    continue
+                    img_links = []
+                # with threads writing data to file
+                self.write_json("file1.json", post_id=post_id, post_text=text)
+                self.write_json("file2.json", post_id=post_id, post_links=links)
+                self.write_json("file3.json", post_id=post_id, post_image_links=img_links)
             except NoSuchElementException:
                 continue
-            data.append({"post_id": post_id, "post_text": text, "post_links": links, "post_image_links": img_links})
-        return data
+
+    def read_json(self, filename):
+        # reading data from json file
+        with open(filename, 'r') as file:
+            return json.load(file)
+
+    def write_json(self, filename, **data):
+        # writing data to json file
+        try:
+            old_data = self.read_json(filename)
+            old_data.append(data)
+            with open(filename, 'r+') as file:
+                file.write(json.dumps(old_data, sort_keys=True, ensure_ascii=False, indent=3, separators=(',', ': ')))
+        except FileNotFoundError:
+            with open(filename, 'w') as file:
+                new_data = []
+                new_data.append(data)
+                file.write(json.dumps(new_data, sort_keys=True, ensure_ascii=False, indent=3, separators=(',', ': ')))
+
+    def update_page(self, interval=0):
+        # updates page every `interval` seconds
+        sleep(interval)
+        self.driver.get(self.URL)
 
     def run(self):
-        """Запуск программы"""
-        driver = webdriver.Firefox(options=self.opts)
-        driver.get(self.URL)
-        result = self.get_info(driver)
-        time_start = datetime.now()
-        if self.is_thread:
-            thread1 = Thread(target=self.save_data_to_json, kwargs={
-                "data": result,
-                "filename": "file1.json",
-                "post_links": False,
-                "post_img_links": False
-            })
-            thread2 = Thread(target=self.save_data_to_json, kwargs={
-                "data": result,
-                "filename": "file2.json",
-                "post_text": False,
-                "post_links": False
-            })
-            thread3 = Thread(target=self.save_data_to_json, kwargs={
-                "data": result,
-                "filename": "file3.json",
-                "post_text": False,
-                "post_img_links": False
-            })
-            thread1.start()
-            thread2.start()
-            thread3.start()
-            thread1.join()
-            thread2.join()
-            thread3.join()
-        else:
-            self.save_data_to_json(result, "file1.json", post_links=False, post_img_links=False)
-            self.save_data_to_json(result, "file2.json", post_links=False, post_text=False)
-            self.save_data_to_json(result, "file3.json", post_text=False, post_img_links=False)
-        print(datetime.now() - time_start)
-        driver.close()
+        # run function
+        self.driver.get(self.URL)
+        self.get_info()
+        self.driver.close()
+        # with another thread reading data from files
+        # log to the file all actions
